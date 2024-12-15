3662108e7554e023cfc74a6903f6a6ae9f511fcb
mhanna <mandzyuk.annie@gmail.com>
2024-12-15 08:04:31 +0100

    fix?

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -1,8 +1,14 @@
-export function About({ title, text }: { title: string, text: string }) {
+interface AboutProps {
+    title: string | null;
+    text: string | null;
+}
+
+
+export function About({ title, text }: AboutProps) {
     return (
           <section className="mt-[40px] flex flex-col gap-[13px]">
-                <h2 className='text-h2 font-bold uppercase'>{title}</h2>
-                <p className='text-body uppercase text-justify'>{text}</p>
+                <h2 className='text-h2 font-bold uppercase'>{title ?? ''}</h2>
+                <p className='text-body uppercase text-justify'>{text ?? ''}</p>
             </section>
     )
 }
